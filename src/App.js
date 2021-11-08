5d906c9ef4f05efce51691749a36b70ae1c139d0
Hieu Le <lehieu.jo@gmail.com>
2021-11-08 10:40:26 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import bob from './public/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div><img src={"/bob.svg"} className="App-logo" alt="logo" />
+        <img src={"/mo.svg"} className="App-logo2" alt="logo" /></div>
+        <p>
+          Em Hà iu ngủ ngon
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
