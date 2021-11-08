d71e1db0ec5d0ece26ede9619133480120c25b18
Hieu Le <lehieu.jo@gmail.com>
2021-11-08 15:52:09 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import bob from './public/logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div><img src={"bob.svg"} className="App-logo" alt="logo" />
-        <img src={"mo.svg"} className="App-logo2" alt="logo" /></div>
+        <div><img src={process.env.PUBLIC_URL + '/bob.png'} className="App-logo" alt="logo" />
+        <img src={process.env.PUBLIC_URL + '/mo.png'} className="App-logo" alt="logo1" /></div>
         <p>
           Em Hà iu ngủ ngon
         </p>
