6f69e7e33efba035a66db5691fada49207d1c738
Hieu Le <lehieu.jo@gmail.com>
2021-11-08 15:30:18 +0100

    a

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div><img src={"/bob.svg"} className="App-logo" alt="logo" />
-        <img src={"/mo.svg"} className="App-logo2" alt="logo" /></div>
+        <div><img src={"bob.svg"} className="App-logo" alt="logo" />
+        <img src={"mo.svg"} className="App-logo2" alt="logo" /></div>
         <p>
           Em Hà iu ngủ ngon
         </p>
