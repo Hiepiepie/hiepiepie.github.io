0821ddfdd1fbb58f383e198693f0ee5f775ab3b2
Hieu Le <lehieu.jo@gmail.com>
2021-11-09 10:34:49 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [className, setclassName] = useState('randomImg');
+  var audio = new Audio('kiss.wav');
+  var audio1 = new Audio('happy.wav');
+
   return (
     <div className="App">
       <header className="App-header">
+     <img src={process.env.PUBLIC_URL + '/hieu.png'}  width='400px' height='400px' className={className} onAnimationEndCapture={()=>{setclassName('stay')
+     audio1.pause();
+     audio.play();
+    }} style={{left : 100, marginTop: -400}}/>
+
+     <img src={process.env.PUBLIC_URL + '/ha.png'}  width='400px' height='400px'  className={className} onAnimationEndCapture={()=>{setclassName('stay')
+    }} style={{right : 100, marginTop: -400}}/>
+
         <div><img src={process.env.PUBLIC_URL + '/bob.png'} className="App-logo" alt="logo" />
         <img src={process.env.PUBLIC_URL + '/mo.png'} className="App-logo" alt="logo1" /></div>
+        
+        
         <p>
-          Em Hà iu ngủ ngon
+          Ấn vào đây này
         </p>
+        <div id="heart" class='button' onClick={()=>{
+          setclassName('dropping')
+          audio1.play()
+        }}></div>
       </header>
     </div>
   );
